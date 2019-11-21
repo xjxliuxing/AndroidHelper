f597b6901058956422407f41adfd1dc8b30d8028
xjxliuxing@163.com <xjxliuxing@163.com>
2019-11-21 23:45:50 +0800

    修改LinearLayout的布局

--- app/src/main/java/com/xjx/helper/MainActivity.java
+++ app/src/main/java/com/xjx/helper/MainActivity.java
@@ -18,29 +18,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        final View viewById = findViewById(R.id.iv_test2);
-        viewById.post(new Runnable() {
-            @Override
-            public void run() {
-
-                int width = viewById.getWidth();
-                Log.e("XJX", "width:Value:" + width);
-
-                int height = viewById.getHeight();
-                Log.e("XJX", "height:Value:" + height);
-
-                int paddingLeft = viewById.getPaddingLeft();
-                int paddingTop = viewById.getPaddingTop();
-
-                Log.e("XJX", "paddingLeft:Value:" + paddingLeft);
-                Log.e("XJX", "paddingTop:Value:" + paddingTop);
-
-                ViewGroup viewById1 = findViewById(R.id.rl_content);
-                int paddingLeft1 = viewById1.getPaddingLeft();
-
-                Log.e("XJX", "paddingLeft---Group---:Value:" + paddingLeft1);
-            }
-        });
+//        final View viewById = findViewById(R.id.iv_test2);
+//        viewById.post(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                int width = viewById.getWidth();
+//                Log.e("XJX", "width:Value:" + width);
+//
+//                int height = viewById.getHeight();
+//                Log.e("XJX", "height:Value:" + height);
+//
+//                int paddingLeft = viewById.getPaddingLeft();
+//                int paddingTop = viewById.getPaddingTop();
+//
+//                Log.e("XJX", "paddingLeft:Value:" + paddingLeft);
+//                Log.e("XJX", "paddingTop:Value:" + paddingTop);
+//
+//                ViewGroup viewById1 = findViewById(R.id.rl_content);
+//                int paddingLeft1 = viewById1.getPaddingLeft();
+//
+//                Log.e("XJX", "paddingLeft---Group---:Value:" + paddingLeft1);
+//            }
+//        });
 
     }
 }

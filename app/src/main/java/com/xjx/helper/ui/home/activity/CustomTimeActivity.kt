f151a35512e12ac201f1ccc5d2a2e6e25fd66cf9
xjxliuxing@163.com <xjxliuxing@163.com>
2020-02-21 20:58:12 +0800

    1

---
+++ app/src/main/java/com/xjx/helper/ui/home/activity/CustomTimeActivity.kt
@@ -0,0 +1,21 @@
+package com.xjx.helper.ui.home.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xjx.helper.R
+import com.xjx.helper.base.CommonBaseTitleActivity
+
+/**
+ * 自定义时间选择器
+ */
+class CustomTimeActivity : CommonBaseTitleActivity() {
+
+    override fun getTitleLayout(): Int {
+        return R.layout.activity_custom_time
+    }
+
+    override fun initData() {
+        super.initData()
+
+    }
+}

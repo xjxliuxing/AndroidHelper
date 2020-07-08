a61d6fe6fec9a3909dd4703b662597c20dec379f
xjxliuxing@163.com <19900713op>
2020-07-08 22:59:01 +0800

    删除无用的类2

--- app/src/main/java/com/xjx/helper/ui/home/fragments/HomeFragment.kt
+++ app/src/main/java/com/xjx/helper/ui/home/fragments/HomeFragment.kt
@@ -1,7 +1,5 @@
 package com.xjx.helper.ui.home.fragments
 
-import android.content.Intent
-import android.view.View
 import com.xjx.helper.R
 import com.xjx.helper.adapter.TestAdapter
 import com.xjx.helper.base.CommonBaseRefreshListFragment
@@ -10,11 +8,9 @@ import com.xjx.helper.http.client.ApiException
 import com.xjx.helper.http.client.ApiServices
 import com.xjx.helper.http.client.BaseResponse
 import com.xjx.helper.http.client.Page
-import com.xjx.helper.ui.DownLoadManagerActivity
 import com.xjx.helper.utils.recyeliview.RecycleUtil
 import kotlinx.android.synthetic.main.fragment_home.*
 import retrofit2.Call
-
 
 /**
  * 首页
@@ -26,7 +22,7 @@ class HomeFragment : CommonBaseRefreshListFragment<StoreActivityBean>() {
     }
 
     override fun onHttpListFailure(e: ApiException?) {
-    
+
     }
 
     override fun getRefreshLayout(): Int {
@@ -43,16 +39,6 @@ class HomeFragment : CommonBaseRefreshListFragment<StoreActivityBean>() {
                 .setAdapter(testAdapter)
 
         setAdapter(testAdapter)
-    }
-
-    override fun onClick(v: View?) {
-        super.onClick(v)
-
-        val intent = Intent()
-        when (v?.id) {
-            R.id.tv_test2 -> intent.setClass(mContext, DownLoadManagerActivity::class.java)
-        }
-        startActivity(intent)
     }
 
     override fun getHttp(): Call<BaseResponse<Page<StoreActivityBean>>> {

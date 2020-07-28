5e0173222ae5025814d82183674c095f22835244
xjxliluxing@163.com <xjxliuxing@163..com>
2020-07-28 15:20:55 +0800

    合并代码3

--- app/src/main/java/com/xjx/helper/ui/home/fragments/TodoFragment.kt
+++ app/src/main/java/com/xjx/helper/ui/home/fragments/TodoFragment.kt
@@ -1,9 +1,14 @@
 package com.xjx.helper.ui.home.fragments
 
+import android.content.Intent
+import android.view.View
 import com.xjx.helper.R
 import com.xjx.helper.base.CommonBaseFragment
-import com.xjx.helper.utils.DialogFragments
-
+import com.xjx.helper.test.TestMapActivity
+import com.xjx.helper.ui.home.activity.tod.animation.AnimationMapActivity
+import com.xjx.helper.ui.home.activity.tod.customview.CustomViewMapActivity
+import com.xjx.helper.ui.home.activity.tod.java.JavaMapActivity
+import com.xjx.helper.ui.home.activity.tod.widget.WidgetMapActivity
 
 /**
  * 待办的fragmen
@@ -14,10 +19,35 @@ class TodoFragment : CommonBaseFragment() {
         return R.layout.fragment_todo
     }
 
-    override fun initData() {
-        super.initData()
+    override fun initListener() {
+        super.initListener()
+        setOnClick(R.id.tv_custom_widget, R.id.tv_custom_view, R.id.tv_animation_map,
+                R.id.tv_java_map, R.id.tv_test_map)
+    }
 
-         DialogFragments.showDialog(childFragmentManager,R.layout.popup_newbie_guide)
+    override fun onClick(v: View?) {
+        super.onClick(v)
+
+        val intent = Intent()
+
+        when (v?.id) {
+            R.id.tv_custom_widget -> {
+                intent.setClass(mContext, WidgetMapActivity::class.java)
+            }
+            R.id.tv_custom_view -> {
+                intent.setClass(mContext, CustomViewMapActivity::class.java)
+            }
+            R.id.tv_animation_map -> {
+                intent.setClass(mContext, AnimationMapActivity::class.java)
+            }
+            R.id.tv_java_map -> {
+                intent.setClass(mContext, JavaMapActivity::class.java)
+            }
+            R.id.tv_test_map -> {
+                intent.setClass(mContext, TestMapActivity::class.java)
+            }
+        }
+        mContext.startActivity(intent)
     }
 
 }

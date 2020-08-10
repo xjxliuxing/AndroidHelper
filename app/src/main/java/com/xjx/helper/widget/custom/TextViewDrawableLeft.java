3d17044bc0c11f8c0b72b938ee33ff654a0b3091
xjxliluxing@163.com <xjxliuxing@163..com>
2020-08-10 18:54:29 +0800

    1111

--- app/src/main/java/com/xjx/helper/widget/custom/TextViewDrawableLeft.java
+++ app/src/main/java/com/xjx/helper/widget/custom/TextViewDrawableLeft.java
@@ -5,11 +5,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.StateListDrawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,27 +17,25 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
-import com.xjx.helper.utils.ConvertUtil;
 import com.xjx.helper.R;
+import com.xjx.helper.utils.ConvertUtil;
 
 public class TextViewDrawableLeft extends View {
 
     private Paint mPaintDrawable = new Paint();
     private Paint mPaintText = new Paint();
     private Bitmap bitmap;
-    private Drawable drawable;
-    private int drawableWidth;
-    private int drawableHeight;
-    private String content;
-    private int color;
-    private float size;
+    private Drawable mDrawable;
+    private int mDrawableWidth;
+    private int mDrawableHeight;
+    private String mContent;
+    private int mColor;
+    private float mSize;
     private int bitmapHeight;
     private int contentWidth;
     private int contentHeight;
-    private int measuredWidth;
-    private int measuredHeight;
-    private int bitmapWidth;
     private int mHeightValue;
+    private float mDrawablePadding;
 
     public TextViewDrawableLeft(Context context) {
         super(context);
@@ -51,21 +49,21 @@ public class TextViewDrawableLeft extends View {
 
     private void initView(Context context, AttributeSet attrs) {
         // 获取属性
-        @SuppressLint("CustomViewStyleable")
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TextViewDrawableLeft);
+        @SuppressLint("CustomViewStyleable") TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TextViewDrawableLeft);
 
         // drawable
-        drawable = array.getDrawable(R.styleable.TextViewDrawableLeft_tdl_drawable);
-        drawableWidth = (int) array.getDimension(R.styleable.TextViewDrawableLeft_tdl_drawable_width, 0);
-        drawableHeight = (int) array.getDimension(R.styleable.TextViewDrawableLeft_tdl_drawable_height, 0);
+        mDrawable = array.getDrawable(R.styleable.TextViewDrawableLeft_tdl_drawable);
+        mDrawableWidth = (int) array.getDimension(R.styleable.TextViewDrawableLeft_tdl_drawable_width, 0);
+        mDrawableHeight = (int) array.getDimension(R.styleable.TextViewDrawableLeft_tdl_drawable_height, 0);
+        mDrawablePadding = array.getDimension(R.styleable.TextViewDrawableLeft_tdl_drawable_padding, 0);
 
         // text
-        content = array.getString(R.styleable.TextViewDrawableLeft_tdl_content);
-        color = array.getColor(R.styleable.TextViewDrawableLeft_tdl_color, ContextCompat.getColor(getContext(), R.color.black_4));
-        size = array.getDimension(R.styleable.TextViewDrawableLeft_tdl_size, ConvertUtil.toSp(16));
+        mContent = array.getString(R.styleable.TextViewDrawableLeft_tdl_content);
+        mColor = array.getColor(R.styleable.TextViewDrawableLeft_tdl_color, ContextCompat.getColor(getContext(), R.color.black_4));
+        mSize = array.getDimension(R.styleable.TextViewDrawableLeft_tdl_size, ConvertUtil.toSp(16));
 
-        mPaintText.setColor(color);
-        mPaintText.setTextSize(size);
+        mPaintText.setColor(mColor);
+        mPaintText.setTextSize(mSize);
         mPaintText.setAntiAlias(true);
 
         mPaintDrawable.setAntiAlias(true);
@@ -78,51 +76,26 @@ public class TextViewDrawableLeft extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        measuredWidth = getMeasuredWidth();
-        measuredHeight = getMeasuredHeight();
+        if (bitmap == null || bitmap.isRecycled()) {
+            bitmap = getBitmap();
+        }
 
         // 获取text的宽高
         if ((contentWidth <= 0) || (contentHeight <= 0)) {
             Rect rect = new Rect();
-            mPaintText.getTextBounds(content, 0, content.length(), rect);
+            mPaintText.getTextBounds(mContent, 0, mContent.length(), rect);
             contentWidth = rect.width();
             contentHeight = rect.height();
         }
 
-        if (bitmap == null) {
-            bitmap = getBitmap();
-        }
-
-        // 获取bitmap的高度
-        if (this.bitmap != null) {
-            bitmapHeight = this.bitmap.getHeight();
-            bitmapWidth = this.bitmap.getWidth();
-        }
-
-        if (contentHeight > bitmapHeight) {
+        // 计算高度
+        if (contentHeight > mDrawableHeight) {
             mHeightValue = contentHeight;
         } else {
-            mHeightValue = bitmapHeight;
+            mHeightValue = mDrawableHeight;
         }
 
-//        int height = mHeightValue;
-//        int width = contentWidth + bitmapWidth;
-//        final int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-//        final int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
-//        final int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
-//        final int heightSpecSize = MeasureSpec.getMode(heightMeasureSpec);
-//        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
-//            setMeasuredDimension(width, height);
-//        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
-//            setMeasuredDimension(width, heightSpecSize);
-//        } else if (heightSpecMode == MeasureSpec.AT_MOST) {
-//            setMeasuredDimension(widthSpecSize, height);
-//        }
-
-        final int width = MeasureSpec.getSize(widthMeasureSpec);
-        final int height = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(resolveSize(width, widthMeasureSpec), resolveSize(height, heightMeasureSpec));
-
+        setMeasuredDimension((int) (contentWidth + mDrawableWidth + mDrawablePadding), mHeightValue);
     }
 
     @Override
@@ -145,7 +118,7 @@ public class TextViewDrawableLeft extends View {
         // 获取text的宽高
         if ((contentWidth <= 0) || (contentHeight <= 0)) {
             Rect rect = new Rect();
-            mPaintText.getTextBounds(content, 0, content.length(), rect);
+            mPaintText.getTextBounds(mContent, 0, mContent.length(), rect);
             contentWidth = rect.width();
             contentHeight = rect.height();
         }
@@ -158,32 +131,37 @@ public class TextViewDrawableLeft extends View {
             top = (contentHeight - bitmapHeight) / 2;
         }
 
-        Rect rectSrc = new Rect(0, top, drawableWidth, drawableHeight);
-        Rect rectDst = new Rect(0, 0, measuredWidth, measuredHeight);
-
+        Rect rectSrc = new Rect(0, 0, mDrawableWidth, mDrawableHeight);
+        Rect rectDst = new Rect(0, 0, mDrawableWidth, mDrawableHeight);
+        // 绘制图片
         canvas.drawBitmap(bitmap, rectSrc, rectDst, mPaintDrawable);
 
-        if (!TextUtils.isEmpty(content)) {
-            Rect rect = new Rect();
-            mPaintText.getTextBounds(content, 0, content.length(), rect);
-            canvas.drawText(content, bitmap.getWidth(), bitmap.getHeight(), mPaintText);
+        // 绘制文字
+        if (!TextUtils.isEmpty(mContent)) {
+            int heightValue = mDrawableHeight + contentHeight;
+            if ((heightValue) > 0) {
+                canvas.drawText(mContent, (bitmap.getWidth() + mDrawablePadding), (heightValue / 2), mPaintText);
+            }
         }
     }
 
     public Bitmap getBitmap() {
-        if (drawable != null) {
-            if (drawable instanceof BitmapDrawable) {
-                bitmap = ((BitmapDrawable) drawable).getBitmap();
-            } else if (drawable instanceof StateListDrawable) {
-                Drawable current = drawable.getCurrent();
-                if (current instanceof BitmapDrawable) {
-                    bitmap = ((BitmapDrawable) current).getBitmap();
-                }
-                bitmap.setWidth(drawableWidth);
-                bitmap.setHeight(drawableHeight);
-            }
+
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.tab_todo_selected);
+        if (drawable instanceof BitmapDrawable) {
+            bitmap = ((BitmapDrawable) drawable).getBitmap();
         }
-        return bitmap;
+
+        int bmpWidth = bitmap.getWidth();
+        int bmpHeight = bitmap.getHeight();
+
+        float scaleWidth = ((float) mDrawableWidth) / bmpWidth;
+        float scaleHeight = ((float) mDrawableHeight) / bmpHeight;
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, bmpWidth, bmpHeight, matrix, true);
     }
 
     public void setBitmap(Bitmap bitmap) {
